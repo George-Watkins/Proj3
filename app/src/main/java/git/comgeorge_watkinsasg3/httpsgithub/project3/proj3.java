163abfe4bc46b493a7e5f9b4b9fb1866476b93e8
George-Watkins <george.watkins.13@cnu.edu>
2016-03-14 20:04:30 -0400

    Have toolbar set but when settings is clicked in the overflow it crashes the app

--- app/src/main/java/git/comgeorge_watkinsasg3/httpsgithub/project3/proj3.java
+++ app/src/main/java/git/comgeorge_watkinsasg3/httpsgithub/project3/proj3.java
@@ -1,8 +1,7 @@
 package git.comgeorge_watkinsasg3.httpsgithub.project3;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -17,15 +16,6 @@ public class proj3 extends AppCompatActivity {
         setContentView(R.layout.activity_proj3);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -42,11 +32,16 @@ public class proj3 extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case R.id.action_settings:
+                doSettings();
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void doSettings() {
+        startActivity(new Intent(this, settings.class));
     }
 }
---
+++ app/src/main/java/git/comgeorge_watkinsasg3/httpsgithub/project3/settings.java
@@ -0,0 +1,13 @@
+package git.comgeorge_watkinsasg3.httpsgithub.project3;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
