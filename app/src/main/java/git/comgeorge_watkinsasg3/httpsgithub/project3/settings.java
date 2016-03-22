a8555b16460fa4e3d37cf68dbc509ee4fb47fbc5
George-Watkins <george.watkins.13@cnu.edu>
2016-03-22 11:46:16 -0400

    The settings page now works.  Text view is clickable and the dialog box pops up with different server options

--- app/src/main/java/git/comgeorge_watkinsasg3/httpsgithub/project3/settings.java
+++ app/src/main/java/git/comgeorge_watkinsasg3/httpsgithub/project3/settings.java
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -20,24 +21,59 @@ public class settings extends Activity {
 
     private RadioGroup radioServerGroup;
     private RadioButton radioServerButton;
+    AlertDialog levelDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        TextView tvButton = (TextView) findViewById(R.id.editText);
 
-        addListener();
-    }
-
-    private void addListener() {
-        radioServerGroup = (RadioGroup) findViewById(R.id.radioServer);
-
-        radioServerGroup.setOnClickListener(new View.OnClickListener() {
+        tvButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int selectedId = radioServerGroup.getCheckedRadioButtonId();
-                Toast.makeText(settings.this, radioServerButton.getText(), Toast.LENGTH_SHORT).show();
+                chooseServer();
             }
         });
     }
+
+    private void chooseServer() {
+
+        final CharSequence[] items = {"CNU - Defender","Pair - Tenton Software - Pets"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Info");
+        builder.setSingleChoiceItems(items, -1, new DialogInterface.OnClickListener(){
+
+            public void onClick(DialogInterface dialog, int item){
+                switch (item){
+                    case 0:
+                        //code when CNU Defender is selected
+                        //Change the url code in proj3.class
+                        break;
+                    case 1:
+                        //code when Pair - Tenton is selected
+                        //change the url code in proj3.code
+                        break;
+                }
+                levelDialog.dismiss();
+            }
+        });
+        levelDialog = builder.create();
+        levelDialog.show();
+    }
+
+    //need to add a dialog box somewhere in here
+//
+//    private void addListener() {
+//        radioServerGroup = (RadioGroup) findViewById(R.id.radioServer);
+//
+//        radioServerGroup.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                int selectedId = radioServerGroup.getCheckedRadioButtonId();
+//                Toast.makeText(settings.this, radioServerButton.getText(), Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//    }
 }
